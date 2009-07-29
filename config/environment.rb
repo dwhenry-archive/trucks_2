dbf0e0e2a1e866d8095774ea2da7b0614c3f1bf5
unknown <Dave@.(none)>
2009-07-30 00:12:41 +0100

    Added GEOKit Implementation

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,12 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   
-  config.gem "geokit"
-  #config.gem "mysql"
+  config.load_paths += Dir["#{RAILS_ROOT}/vendor/gems/**"].map do |dir|
+    File.directory?(lib = "#{dir}/lib") ? lib : dir
+  end
+ # require "geokit"
+
+ config.gem "geokit"
   
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
